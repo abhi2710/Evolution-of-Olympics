4ceb46cf944ef08a6d2ce67935eaa4facc55f401
Abhinav <abhinav@abhinavs-mbp.uhmc.sbuh.stonybrook.edu>
2019-04-15 21:57:23 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+import json
+import pandas
+
+
+# CREATE FLASK APP INSTANCE
+app = Flask("Evolution-of-Olympics")  # creates the Flask instance
+data = []
+
+
+# LOAD DATA FROM CSV
+def load_data():
+    global data
+    data = pandas.read_csv('Crime.csv', low_memory=False)
+    print("-----------DATA LOADED--------------")
+
+
+#########################
+###### ROUTES ###########
+#########################
+
+
+# LANDING PAGE ROUTE
+@app.route("/")
+def landing_page():
+    global data
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
