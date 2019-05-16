abc156cbd376d246ccacf7dd1b9f98860f45082d
Abhinav <abhinav@abhinavs-mbp.uhmc.sbuh.stonybrook.edu>
2019-05-16 19:00:58 -0400

    global year

--- static/js/index.js
+++ static/js/index.js
@@ -1,43 +1,21 @@
 console.log("JS Loaded");
-
-function calculate_year(year_idx) {
-	let year;
-	year_idx = parseInt(year_idx);
-	if (year_idx < 7) {
-		// oly summer games for the first 7 years
-		year = 1896 + year_idx * 4;
-	} else if (year_idx < 43) {
-		// introduction of winter games
-		year_idx -= 7;
-		year = 1924 + Math.floor(year_idx/2) * 4;
-	} else {
-		// splitting of summer and winter games
-		// two years apart now from 1994 onwards.
-		year_idx -= 43;
-		year = 1994 + year_idx * 2;
-	}
-	if (year === 1916 || year === 1940 || year === 1944) {
-		return -1;
-	} else if (year > 2016) {
-		return -2;
-	}
-	return year;
-}
+let bubbleSVG;
 
 $(function() {
-	let season_slider  = $("#season-slider");
-	let bubbleSVG = d3.select("#bubble-chart")
+	bubbleSVG = d3.select("#bubble-chart")
 				.append('svg')
 				.classed('svg-content', true);
-	let year_idx = season_slider[0].value;
-
-	season_slider.on('change', function () {
-		console.log(this.value);
-		let year = calculate_year(this.value);
-		init_participation_bubble(bubbleSVG, year, init_participation_bar);
-	});
-
-	let year = calculate_year(year_idx);
-	init_participation_bubble(bubbleSVG, year, init_participation_bar);
-
+	console.log("init");
+	init_participation_bubble(bubbleSVG, yearSelected, init_participation_bar);
 });
+
+
+//UPDATE ALL MAPS
+let updateMaps = (year)=>{
+    if(year == yearSelected){
+        return
+    }
+    yearSelected = year;
+	console.log("update");
+		init_participation_bubble(bubbleSVG, year, init_participation_bar);
+}
